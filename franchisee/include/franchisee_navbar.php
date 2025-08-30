ac7bda5e9e2b31beadc57c80f76a6b785afb7edf
LuizianeBrantaKent <k210302@student.kent.edu.au>
2025-08-30 13:15:24 +1000

    created new files

--- franchisee/include/franchisee_navbar.php
+++ franchisee/include/franchisee_navbar.php
@@ -1,49 +1,49 @@
-<div class="admin-navbar">
+<div class="franchisee-navbar">
     <ul class="nav-menu">
         <li class="nav-item">
-            <a href="../admin/admin_dash.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_dash.php') echo 'active'; ?>">
+            <a href="../franchisee/franchisee_dash.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='franchisee_dash.php') echo 'active'; ?>">
                 <i class="fas fa-th-large"></i>
                 <span class="nav-text">Dashboard</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="../admin/admin_manage_appointments.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_manage_appointments.php') echo 'active'; ?>">
+            <a href="../franchisee/messaging.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='franchisee/messaging.php') echo 'active'; ?>">
                 <i class="fas fa-calendar-check"></i>
-                <span class="nav-text">Manage Appointments</span>
+                <span class="nav-text">Messages</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="../admin/admin_manage_inventory.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_manage_inventory.php') echo 'active'; ?>">
+            <a href="../franchisee/training.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='training.php') echo 'active'; ?>">
                 <i class="fas fa-boxes"></i>
-                <span class="nav-text">Manage Inventory</span>
+                <span class="nav-text">Training</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="../admin/admin_manage_users.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_manage_users.php') echo 'active'; ?>">
+            <a href="../franchisee/documents.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='documents.php') echo 'active'; ?>">
                 <i class="fas fa-users"></i>
-                <span class="nav-text">Manage Users</span>
+                <span class="nav-text">Documents</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="../admin/admin_announcements.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_announcements.php') echo 'active'; ?>">
+            <a href="../franchisee/reports.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='reports.php') echo 'active'; ?>">
                 <i class="fas fa-bullhorn"></i>
-                <span class="nav-text">Communication</span>
+                <span class="nav-text">Reports</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="../admin/admin_reports.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_reports.php') echo 'active'; ?>">
+            <a href="../franchisee/admin_reports.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_reports.php') echo 'active'; ?>">
                 <i class="fas fa-chart-bar"></i>
                 <span class="nav-text">Reports</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="../admin/admin_uploads.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_uploads.php') echo 'active'; ?>">
+            <a href="../franchisee/admin_uploads.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_uploads.php') echo 'active'; ?>">
                 <i class="fas fa-file-upload"></i>
                 <span class="nav-text">Upload Training & Documents</span>
             </a>
         </li>
         <li class="nav-item">
-            <a href="../admin/admin_profile.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_profile.php') echo 'active'; ?>">
+            <a href="../franchisee/admin_profile.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=='admin_profile.php') echo 'active'; ?>">
                 <i class="fas fa-user-cog"></i>
                 <span class="nav-text">Profile / Settings</span>
             </a>
